fd9b046cf21b8ac6a3fb6e71e59e2f920164b351
Ne184 <nevena.gincheva@abv.bg>
2022-02-05 14:31:46 +0100

    Added bananas to the monkeys

--- src/js/Application.js
+++ src/js/Application.js
@@ -13,9 +13,21 @@ export default class Application extends EventEmitter {
     this.banana = "🍌";
     this.emit(Application.events.READY);
   }
+
   setEmojis(emojis) {
     this.emojis = emojis;
+    let modifiedEmojis = this.addBananas();
+    let paragraph = document.createElement('p');
+    const div = document.querySelector("div");
+    paragraph.textContent = modifiedEmojis;
+    div.appendChild(paragraph);
   }
 
-  addBananas() {}
+  addBananas() {
+    // this.setEmojis(["monkey1","monkey2"]);
+    let modifiedArray = (this.emojis).map((e) =>{
+      return e + "🍌";
+    });
+    return modifiedArray;
+  }
 }
